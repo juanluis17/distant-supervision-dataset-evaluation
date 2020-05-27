ad3271e277ad7d1c81a3bd7d7b4c42488ab431ab
Juan Luis <juanluis1702@gmail.com>
2020-05-26 23:54:13 -0500

    Readme

--- run_all.sh
+++ run_all.sh
@@ -6,10 +6,10 @@ END=41
 for i in $(seq 0 $END);
 do
   echo $i
-  python reside.py -data ./data/riedel_original.pkl -name reside_original_$i
-  python pcnnatt.py -data ./data/riedel_original.pkl -name pcnnatt_original_$i -attn
-  python pcnnatt.py -data ./data/riedel_original.pkl -name pcnn_original_$i
-  python cnnatt.py -data ./data/riedel_original.pkl -name cnnatt_original_$i -attn
-  python cnnatt.py -data ./data/riedel_original.pkl -name cnn_original_$i
-  python bgwa.py -data ./data/riedel_original.pkl -name bgwa_original_$i
+  python reside.py -data ./data/riedel_train.pkl -name reside_original_$i
+  python pcnnatt.py -data ./data/riedel_train.pkl -name pcnnatt_original_$i -attn
+  python pcnnatt.py -data ./data/riedel_train.pkl -name pcnn_original_$i
+  python cnnatt.py -data ./data/riedel_train.pkl -name cnnatt_original_$i -attn
+  python cnnatt.py -data ./data/riedel_train.pkl -name cnn_original_$i
+  python bgwa.py -data ./data/riedel_train.pkl -name bgwa_original_$i
 done
